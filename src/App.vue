23cd3f1e1f43fe37dd62d690f5203a571e56c2bf
sagar <sagar@bitmascot.com>
2023-02-18 19:23:04 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from "vue"
+const count = ref(0);
+
+console.log(count);
+
+const addToCount = () => {
+  count.value++;
+}
+
+const subtractFromCount = () => {
+  count.value--;
+}
+</script>
+
+<!--<script>-->
+<!--export default({-->
+<!--  data() {-->
+<!--    return {-->
+<!--      count: 0-->
+<!--    }-->
+<!--  },-->
+<!--  methods: {-->
+<!--    addToCount(){-->
+<!--      this.count = this.count + 1;-->
+<!--    },-->
+<!--    subtractFromCount() {-->
+<!--      this.count = this.count - 1;-->
+<!--    }-->
+<!--  }-->
+<!--})-->
+<!--</script>-->
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{count}}</h1>
+      <button @click="subtractFromCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+div {
+  text-align: center;
+}
+</style>
